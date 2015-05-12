ba3100c04a02c77a4e267b9cf374d868798c0fcb
tOverney <tristan.overney@epfl.ch>
2015-05-12 15:52:54 +0200

    added a rudimentary build script and java export of the PDE

---
+++ ImgProc/build.sh
@@ -0,0 +1,2 @@
+javac -classpath /opt/homebrew-cask/Caskroom/processing/2.2.1/Processing.app/Contents/Java/core.jar -g $1.java
+java -classpath /opt/homebrew-cask/Caskroom/processing/2.2.1/Processing.app/Contents/Java/core.jar:. $1
